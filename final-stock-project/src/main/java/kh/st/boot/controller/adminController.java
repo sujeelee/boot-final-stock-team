ff6302acd28e10b779b5e582f32dff712c7443e5
wlgns647 <wlgns64711234@gmail.com>
2024-10-01 00:06:47 +0900

    작업중

--- final-stock-project/src/main/java/kh/st/boot/controller/adminController.java
+++ final-stock-project/src/main/java/kh/st/boot/controller/adminController.java
@@ -1,8 +1,13 @@
 package kh.st.boot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class adminController {
 
+	  @GetMapping("/news")
+	    public String newsPage() {
+	        return "/admin/news";  // admin/news.html로 이동
+	    }
 }
