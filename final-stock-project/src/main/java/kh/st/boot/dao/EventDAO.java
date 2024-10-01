67c0fcbdb4c841fe6ce911fcc4252fb767e7a65d
cocoparoma <bnbz201@naver.com>
2024-10-01 14:55:10 +0900

    get Event list

--- final-stock-project/src/main/java/kh/st/boot/dao/EventDAO.java
+++ final-stock-project/src/main/java/kh/st/boot/dao/EventDAO.java
@@ -1,6 +1,11 @@
 package kh.st.boot.dao;
 
+import java.util.List;
+
+import kh.st.boot.model.dto.EventDTO;
+
 public interface EventDAO {
 
+    List<EventDTO> getEventList(String eventStatus);
     
 }
--- final-stock-project/src/main/java/kh/st/boot/service/EventServiceImp.java
+++ final-stock-project/src/main/java/kh/st/boot/service/EventServiceImp.java
@@ -1,8 +1,11 @@
 package kh.st.boot.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import kh.st.boot.dao.EventDAO;
+import kh.st.boot.model.dto.EventDTO;
 import lombok.AllArgsConstructor;
 
 @Service
@@ -10,6 +13,13 @@ import lombok.AllArgsConstructor;
 public class EventServiceImp implements EventService {
 
     private EventDAO eventDao;
+
+    @Override
+    public List<EventDTO> getEventList(String eventStatus) {
+        return eventDao.getEventList(eventStatus);
+    }
+
+
 
     
 }
