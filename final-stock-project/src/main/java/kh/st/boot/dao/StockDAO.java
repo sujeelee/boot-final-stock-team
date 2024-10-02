fc15ed494a5030cb8dc07596745207bdb9e7a8d0
sujeelee <sujee54@gmail.com>
2024-10-02 17:18:54 +0900

    주식 기능 추가

---
+++ final-stock-project/src/main/java/kh/st/boot/dao/StockDAO.java
@@ -0,0 +1,32 @@
+package kh.st.boot.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import kh.st.boot.model.vo.StockPriceVO;
+import kh.st.boot.model.vo.StockVO;
+import kh.st.boot.pagination.Criteria;
+import kh.st.boot.pagination.StockCriteria;
+
+public interface StockDAO {
+
+	StockVO getStockCompany(@Param("st_code")String st_code);
+	
+	void companyType(@Param("st_code")String st_code, @Param("st_type")String st_type, @Param("st_status")String st_status);
+
+	void insertStockCompany(@Param("st")StockVO newStock);
+	
+	List<StockVO> getCompanyList(@Param("type")String type, @Param("cri")StockCriteria cri);
+
+	StockPriceVO getStockPrice(@Param("si_date")String si_date, @Param("st_code")String st_code);
+
+	boolean insertStockPrice(@Param("si")StockPriceVO price);
+
+	int getCount(@Param("cri")StockCriteria cri);
+	
+	List<StockPriceVO> getStockInfoList(@Param("st_code")String st_code);
+
+	int getCountStockPrice(@Param("st_code")String st_code);
+
+}
