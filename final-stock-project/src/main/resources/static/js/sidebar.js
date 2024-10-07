ae36d86b2893ed544b21b18d6f68efa0a8dc0bb3
parkgwanggyun <sonud_1@naver.com>
2024-10-07 17:01:13 +0900

    사이드바 수정

--- final-stock-project/src/main/resources/static/js/sidebar.js
+++ final-stock-project/src/main/resources/static/js/sidebar.js
@@ -1,26 +1,22 @@
-$(document).ready(function() {
-  $("#btn").on("click", function() {
-    $(".sidebar").toggleClass("open");
-    menuBtnChange(); // calling the function (optional)
-    adjustLoginButton(); // 로그인 버튼 위치 조정
-  });
+function showDash(icon) {
+    if (!$(icon).hasClass("active")) {
+        // 열릴 때 처리
+        $(".test").css("display", "block"); // 먼저 display를 block으로 변경
+        setTimeout(function() {
+            $(".test").css("width", "200px"); // 너비를 200px로 변경 (애니메이션 적용)
+        }, 10); // 작은 지연 시간을 두어 자연스러운 애니메이션을 보장
 
-  // 로그인 버튼 위치 조정 함수
-  function adjustLoginButton() {
-    if ($(".sidebar").hasClass("open")) {
-      $(".login").css("padding-right", "200px"); // 사이드바 열릴 때 패딩값 줄이기
+        $(".side_icon").removeClass("active");
+        $(icon).addClass("active");
     } else {
-      $(".login").css("padding-right", "0px"); // 사이드바 닫힐 때 원래 패딩으로 돌아가기
-    }
-  }
+        // 닫힐 때 처리
+        $(".test").css("width", "0px"); // 너비를 0으로 설정하여 닫히는 애니메이션
+        $(".side_icon").removeClass("active");
+        $(icon).removeClass("active");
 
-  // following are the code to change sidebar button (optional)
-  function menuBtnChange() {
-    if ($(".sidebar").hasClass("open")) {
-      $(".sidebar").removeClass("bx-menu").addClass("bx-menu-alt-right"); // replacing the icons class
-    } else {
-      $(".sidebar").removeClass("bx-menu-alt-right").addClass("bx-menu"); // replacing the icons class
+        // 애니메이션이 끝난 후 요소를 숨김
+        setTimeout(function() {
+            $(".test").css("display", "none"); // 애니메이션 완료 후 숨기기
+        }, 250); // 애니메이션 시간과 일치시키기
     }
-  }
-});
-
+}
