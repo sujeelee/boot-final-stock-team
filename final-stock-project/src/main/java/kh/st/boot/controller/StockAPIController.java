f16544532bd179b6e436adcb9faa0228e77ed2b0
parkgwanggyun <sonud_1@naver.com>
2024-10-08 14:11:51 +0900

    사이드바 애니메이션 추가 수정보완필요

--- final-stock-project/src/main/java/kh/st/boot/controller/StockAPIController.java
+++ final-stock-project/src/main/java/kh/st/boot/controller/StockAPIController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
