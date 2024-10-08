aeddfef1c4eef1f52ec36329d7d657756c8f2d3b
cocoparoma <bnbz201@naver.com>
2024-10-08 11:33:45 +0900

    임포트

--- final-stock-project/src/main/java/kh/st/boot/controller/StockAPIController.java
+++ final-stock-project/src/main/java/kh/st/boot/controller/StockAPIController.java
@@ -35,6 +35,7 @@ import kh.st.boot.model.vo.StockPriceVO;
 import kh.st.boot.model.vo.StockVO;
 import kh.st.boot.pagination.Criteria;
 import kh.st.boot.pagination.PageMaker;
+import kh.st.boot.pagination.StockCriteria;
 import kh.st.boot.service.StockService;
 
 
