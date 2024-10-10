1851508230b8913580c7e28dfe7d1fb376447b3b
sulhyun <sulhyun4788@naver.com>
2024-10-10 14:28:26 +0900

    [hyun] 수익률 계산 완료~

--- final-stock-project/src/main/java/kh/st/boot/controller/MyAccountController.java
+++ final-stock-project/src/main/java/kh/st/boot/controller/MyAccountController.java
@@ -1,25 +1,56 @@
 package kh.st.boot.controller;
 
+import java.security.Principal;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kh.st.boot.model.util.DateUtil;
+import kh.st.boot.model.vo.AccountVO;
+import kh.st.boot.model.vo.DepositVO;
+import kh.st.boot.service.MyAccountService;
 import lombok.AllArgsConstructor;
 
 @Controller
 @AllArgsConstructor
 @RequestMapping("/myaccount")
 public class MyAccountController {
-
+	
+	private MyAccountService myAccountService;
+	
 	@GetMapping("/asset")
-	public String asset() {
-
+	public String asset(Model model, Principal principal) {
+		String mb_id = principal.getName();
+		// 회원 아이디로 예치금 잔액을 가져오는 코드
+		AccountVO account = myAccountService.getAccountById(mb_id);
+		String lastWeek = DateUtil.getLastWeek(); // 지난주 날짜 정보
+		// 일주일 전까지의 거래 내역을 가져옴
+		List<DepositVO> depositList = myAccountService.getDepositListByDate(mb_id, lastWeek);
+		int money = 0;
+		for(DepositVO deposit : depositList) {
+			money += deposit.getDe_no();
+		}
+		// 수익률
+		double rateOfReturn = (double)(account.getAc_deposit() - (account.getAc_deposit() - money)) / (account.getAc_deposit() - money) * 100;
+		model.addAttribute("account", account);
+		model.addAttribute("money", money);
+		model.addAttribute("rateOfReturn", rateOfReturn);
 		return "myaccount/asset";
 	}
 	
 	@GetMapping("/transactions")
-	public String transactions() {
-		
+	public String transactions(Model model, Principal principal) {
+		String mb_id = principal.getName();
+		// 거래내역을 가져오는 코드
+		List<DepositVO> list = myAccountService.getDepositList(mb_id);
+		model.addAttribute(list);
 		return "myaccount/transactions";
 	}
 	
