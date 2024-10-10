90b25885df14ae6c167ad100edb6f0d8c89d9ce8
sujeelee <sujee54@gmail.com>
2024-10-10 17:45:44 +0900

    마이페이지 사이드바 추가 / 거래 내역 작업중

---
+++ final-stock-project/src/main/java/kh/st/boot/service/TransService.java
@@ -0,0 +1,33 @@
+package kh.st.boot.service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import kh.st.boot.dao.DepositDAO;
+import kh.st.boot.dao.MemberDAO;
+import kh.st.boot.model.vo.AccountVO;
+import kh.st.boot.model.vo.DepositOrderVO;
+import kh.st.boot.model.vo.DepositVO;
+import kh.st.boot.model.vo.MemberVO;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor	
+public class TransService {
+	private DepositDAO depositDao;
+	private MemberDAO memberDao;
+	public AccountVO getAccountAmt(String mb_id) {
+		
+		MemberVO mb = memberDao.findById(mb_id);
+		
+		if(mb == null) return null;
+		
+		AccountVO ac = depositDao.getAccount(mb.getMb_no());
+		
+		return ac;
+	}
+	
+}
