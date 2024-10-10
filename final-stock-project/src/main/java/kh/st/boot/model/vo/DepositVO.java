0a73160384394ca3c652431ad671868484fb27cb
sujeelee <sujee54@gmail.com>
2024-10-10 15:33:52 +0900

    예치금 충전 완료

---
+++ final-stock-project/src/main/java/kh/st/boot/model/vo/DepositVO.java
@@ -0,0 +1,17 @@
+package kh.st.boot.model.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class DepositVO {
+	private int de_no;//예치금 로그 기본키
+	private String de_content; //예치금 정보
+	private Date de_datetime; //실행된 일자
+	private String de_stock_code; //주식을 샀다면?
+	private String mb_id; //회원아이디
+	private int de_num; //사용/충전된 예치금가
+}
