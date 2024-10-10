4183bb60307e1fdb54aed2d729214d6aac4956a4
sulhyun <sulhyun4788@naver.com>
2024-10-10 19:28:36 +0900

    [hyun] 마이페이지(자산) 그래프 구현중

--- final-stock-project/src/main/java/kh/st/boot/config/MailConfig.java
+++ final-stock-project/src/main/java/kh/st/boot/config/MailConfig.java
@@ -17,7 +17,7 @@ public class MailConfig {
         mailSender.setPort(587); // 구글은 587 사용
 
         mailSender.setUsername("s2dkey.company@gmail.com"); 
-        mailSender.setPassword(""); //앱비밀번호는 직접 입력해서 쓰기  
+        mailSender.setPassword("pbfq nnxe yzyu knfu"); //앱비밀번호는 직접 입력해서 쓰기  
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
