a905c1d22c3511bcee84816337a40a5f904a8524
sujeelee <sujee54@gmail.com>
2024-10-10 17:45:44 +0900

    마이페이지 사이드바 추가 / 거래 내역 작업중

--- final-stock-project/src/main/java/kh/st/boot/controller/TransactionsController.java
+++ final-stock-project/src/main/java/kh/st/boot/controller/TransactionsController.java
@@ -8,8 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kh.st.boot.model.vo.AccountVO;
 import kh.st.boot.model.vo.DepositVO;
 import kh.st.boot.service.MyAccountService;
+import kh.st.boot.service.TransService;
 import lombok.AllArgsConstructor;
 
 @Controller
@@ -17,21 +19,26 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/myaccount/transactions")
 public class TransactionsController {
 	
+	private TransService transService;
 	private MyAccountService myAccountService;
 	
 	@GetMapping("")
 	public String transactions(Model model, Principal principal) {
-		if(principal == null) {
+		/*if(principal == null) {
 			model.addAttribute("msg", "회원만 이용가능합니다.\n로그인 페이지로 이동합니다.");
         	model.addAttribute("url", "/member/login");
         	
             return "util/msg";
 		}
-		String mb_id = principal.getName();
+		String mb_id = principal.getName();*/
+		String mb_id = "zelda1234"; //test용
 		// 거래내역을 가져오는 코드
 		List<DepositVO> list = myAccountService.getDepositList(mb_id);
-		System.out.println(list);
-		model.addAttribute(list);
+		//계좌 잔액을 가져오게 되
+		AccountVO ac = transService.getAccountAmt(mb_id);
+		
+		model.addAttribute("account", ac);
+		model.addAttribute("list", list);
 		return "myaccount/transactions";
 	}
 }
