5505a5a03e95291ed4eb8b48329ea68ab442d584
parkgwanggyun <sonud_1@naver.com>
2024-10-17 17:05:39 +0900

    커뮤니티 제작중

--- final-stock-project/src/main/java/kh/st/boot/service/CommunityService.java
+++ final-stock-project/src/main/java/kh/st/boot/service/CommunityService.java
@@ -1,15 +1,51 @@
 package kh.st.boot.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import kh.st.boot.dao.CommunityDAO;
 import kh.st.boot.dao.MemberDAO;
+import kh.st.boot.model.vo.BoardVO;
+import kh.st.boot.model.vo.CommentVO;
+import kh.st.boot.model.vo.CommunityActionVO;
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class CommunityService {
-	
-	private MemberDAO memberDao;
-	private CommunityDAO communityDAO;
+
+    private CommunityDAO communityDAO;
+    
+//    public void addLike(int wr_no) {
+//        communityDAO.incrementLikes(wr_no);
+//    }
+//
+//    public void addReport(int wr_no) {
+//        communityDAO.incrementReports(wr_no);
+//    }
+//
+//    public void addComment(CommentVO comment) {
+//        communityDAO.addComment(comment);
+//    }
+
+	public void insertBoard(BoardVO newBoard) {
+		communityDAO.insertBoard(newBoard);		
+	}
+
+	public List<BoardVO> getBoardList(String st_code) {
+		if(st_code == null) {
+			return null;
+		}
+		return communityDAO.getBoardList(st_code);
+	}
+
+	public CommunityActionVO nuguaction(Integer wr_no, String mb_id) {
+        CommunityActionVO action = communityDAO.nuguaction(wr_no, mb_id);
+       
+        if (action == null) {
+            return null;
+        }
+        return action;
+    }
 }
