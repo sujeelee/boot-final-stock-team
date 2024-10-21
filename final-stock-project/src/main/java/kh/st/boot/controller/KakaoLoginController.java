ad139f7d4d118be7a6c1b204e16060722244b038
cocoparoma <bnbz201@naver.com>
2024-10-21 17:28:32 +0900

    카카오로그인 완료 네이버 API읽는중

--- final-stock-project/src/main/java/kh/st/boot/controller/KakaoLoginController.java
+++ final-stock-project/src/main/java/kh/st/boot/controller/KakaoLoginController.java
@@ -1,18 +1,17 @@
 package kh.st.boot.controller;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import kh.st.boot.info.KakaoUserInfo;
 import kh.st.boot.service.KakaoService;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class KakaoLoginController {
 
     private final KakaoService kakaoService;
@@ -20,17 +19,13 @@ public class KakaoLoginController {
     @GetMapping("/oauth/kakao/callback")
     public String callback(@RequestParam("code") String code, Model mo) {
 
-        String accessToken = kakaoService.getAccessTokenFromKakao(code);
+        String accessToken = kakaoService.getAccessTokenFromKakao(code);        
+        KakaoUserInfo userInfo = kakaoService.getUserInfo(accessToken); 
+
         System.out.println("accessToken: " + accessToken);
-
-        KakaoUserInfo userInfo = kakaoService.getUserInfo(accessToken);
         System.out.println("userInfo: " + userInfo.getKakaoAccount().getEmail());
-
-
+        mo.addAttribute("userInfo", userInfo.getKakaoAccount().getEmail());
         
-        return "";
-        // return ResponseEntity.ok()
-        // .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken) // 필요시 토큰을 헤더에 포함
-        // .body(userInfo); // 사용자 정보를 반환
+        return "member/join"; // join.html로 렌더링
     }
 }
