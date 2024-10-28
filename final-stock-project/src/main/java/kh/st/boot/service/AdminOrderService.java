b4cb8971b2ccc5973bf156281e2017421b18264b
oiihyg <oiihyg217@gmail.com>
2024-10-28 12:53:45 +0900

    브런치로

--- final-stock-project/src/main/java/kh/st/boot/service/AdminOrderService.java
+++ final-stock-project/src/main/java/kh/st/boot/service/AdminOrderService.java
@@ -9,13 +9,10 @@ import kh.st.boot.dao.AdminOrderDAO;
 import kh.st.boot.dao.NewspaperDAO;
 import kh.st.boot.model.vo.NewsPaperVO;
 import kh.st.boot.model.vo.admOrderPageVO;
-<<<<<<< HEAD
 import kh.st.boot.pagination.Criteria;
 import kh.st.boot.pagination.OrderCriteria;
 import kh.st.boot.pagination.PageMaker;
 import kh.st.boot.pagination.UserCriteria;
-=======
->>>>>>> parent of 0165321 (주문페이지 페이지네이션)
 
 @Service
 public class AdminOrderService {
@@ -24,13 +21,8 @@ public class AdminOrderService {
 	private AdminOrderDAO adminOrderDAO ;
 
 
-<<<<<<< HEAD
 	public List<admOrderPageVO> getAllsltAdminOrder(Criteria cri) {
 		return adminOrderDAO.selectAlladminOrder(cri);
-=======
-	public List<admOrderPageVO> getAllsltAdminOrder() {
-		return adminOrderDAO.selectAlladminOrder();
->>>>>>> parent of 0165321 (주문페이지 페이지네이션)
 	}
 
 	public PageMaker getPageMaker(Criteria cri) {
@@ -47,7 +39,6 @@ public class AdminOrderService {
 		return adminOrderDAO.deletOrder(od_id);
 	}
 
-<<<<<<< HEAD
 	public List<admOrderPageVO> getOrderSearch(String od_sh, OrderCriteria cri) {
 		return adminOrderDAO.orderSearch(od_sh, cri);
 	}
@@ -57,8 +48,6 @@ public class AdminOrderService {
 	        return new PageMaker(10, cri, totalCount); // PageMaker 생성 (displayPageNum을 10으로 설정)
 	    }
 
-=======
->>>>>>> parent of 0165321 (주문페이지 페이지네이션)
 
 
 	
