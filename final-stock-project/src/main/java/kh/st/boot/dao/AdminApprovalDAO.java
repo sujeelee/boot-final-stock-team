4ad077b8b91187a401ee58966b2548ea16157a23
oiihyg <oiihyg217@gmail.com>
2024-10-30 14:00:48 +0900

    관리자 페이지 DAO업로드

--- final-stock-project/src/main/java/kh/st/boot/dao/AdminApprovalDAO.java
+++ final-stock-project/src/main/java/kh/st/boot/dao/AdminApprovalDAO.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import kh.st.boot.model.vo.AdmApprovalVO;
+import kh.st.boot.pagination.Criteria;
 
 @Mapper
 public interface AdminApprovalDAO {
 
-	List<AdmApprovalVO> nullSelectAll();
+	List<AdmApprovalVO> nullSelectAll(Criteria cri);
 
 	List<AdmApprovalVO> updateApprove(int mp_no, String mp_yn);
 
@@ -18,5 +19,7 @@ public interface AdminApprovalDAO {
 	void newsInsert(int mb_no, String mp_company);
 
 	void stockInsert(int mb_no, String mp_company);
+
+	int selectCountList(Criteria cri);
 
 }
